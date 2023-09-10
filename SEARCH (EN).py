4d4606329d1b71373d9507b1ee2409cf821bcd4f
Bank848 <65855292+Bank848@users.noreply.github.com>
2023-09-10 12:33:30 +0700

    Update SEARCH (EN).py

--- SEARCH (EN).py
+++ SEARCH (EN).py
@@ -1,57 +1,55 @@
 import os
+import re  # You need to import this for re.split
 from tqdm import tqdm
+
+# This function is used for natural sorting
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    return [atoi(c) for c in re.split(r'(\d+)', text)]
 
 # Code from ChatGPT
 # Made By Bank's : Thai translator H Game
 # Link Discord : https://discord.gg/q6FkGCHv66
 
 # Set the folder paths you want to search in
-# F:\Folder\Raw text
-folder_paths = [r'F:\Folder\Raw text']
+# F:\FOLDER\TEXTFLIE
+folder_paths = [r'F:\SUMMER ALL TRANSLATE\All monob plus level']
 
 while True:  # This loop will allow the user to keep searching
-    # Input the keyword to search for
     keyword = input("Enter the keyword to search for: ")
 
-    # Initialize a variable to indicate whether the keyword is found
     found = False
-
-    # Create a list to store all the files to be searched
     files_to_search = []
-    matching_files = []  # To store the files where the keyword is found
+    matching_files = []
 
-    # Iterate over each specified directory
     for folder_path in folder_paths:
-        # Iterate over each file in the directory
         for filename in os.listdir(folder_path):
-            # Check if the file is a .txt file
             if filename.endswith('.txt'):
                 file_path = os.path.join(folder_path, filename)
                 files_to_search.append(file_path)
 
-    # Iterate over each file in files_to_search with a progress bar
     for file_path in tqdm(files_to_search, desc="Searching files", unit="file", position=0, leave=True):
-        # Open and read the file
         with open(file_path, 'r', encoding='utf8') as f:
             content = f.read()
-
-            # If the keyword is found in the file
             if keyword in content:
                 matching_files.append(file_path)
                 found = True
 
     # Display results after the progress bar
-    # Use tqdm to wrap the matching_files list during sorting
-    sorted_files = sorted(tqdm(matching_files, desc="Sorting files", position=1, leave=True), key=lambda x: os.path.basename(x))
-    for match in sorted_files:
+    # Sort the matching_files list using natural sorting
+    for _ in tqdm(matching_files, desc="Sorting files", unit="file", position=1, leave=True):  # progress bar for sorting
+        matching_files.sort(key=lambda x: natural_keys(os.path.basename(x)))
+
+    # Then, display the sorted files
+    for match in matching_files:
         _, filename = os.path.split(match)
         print(f"The keyword '{keyword}' was found in the file: {filename}")
 
-    # If the keyword wasn't found in any file
     if not found:
         print(f"\nThe keyword '{keyword}' was not found in any file.")
 
-    # Ask the user if they want to search again
     choice = input("Do you want to search again? (yes/no): ").lower()
     if choice != 'yes':
-        break  # Exit the loop and end the program
+        break
