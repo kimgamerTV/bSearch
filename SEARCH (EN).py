31aeab31b87b8d71f40c227aa1e0345c8c122cb7
Bank848 <65855292+Bank848@users.noreply.github.com>
2023-09-26 18:25:38 +0700

    Update SEARCH (EN).py

--- SEARCH (EN).py
+++ SEARCH (EN).py
@@ -10,7 +10,7 @@ import subprocess
 # สามารถหาคำไทยได้แต่ต้องรันโค้ดผ่านโปรแกรมรันโค้ด เช่น Visual Studio Code
 
 # How to use
-# Just input Folder paths, the keyword you want to search, and the file extension
+# Just input Folder paths, the keyword you want to search, the replacement keyword, and the file extension
 # No need to edit the folder address in the code, you can run the program and use it right away.
 
 # วิธีใช้
@@ -57,23 +57,31 @@ print("ไม่ต้องแก้ที่อยู่โฟลเดอร�
 print("")
 print("Please set all Folder names to English first.")
 print("Enter the folder paths you want to search in. Type 'done' when finished:")
-    
+
+def to_raw_string(s):
+    return s.encode('unicode_escape').decode()
+
 while True:
-    path = to_raw_string(input("Enter folder path (or 'done' to finish): "))
+    path = input("Enter folder path (or 'done' to finish): ")
+    # Check if user hasn't added any paths yet and tries to exit
+    if path.lower() == 'done' and not folder_paths:
+        print("You haven't added any folder paths yet. Please add at least one folder path.")
+        continue
+    elif path.lower() == 'done':
+        break
     
-    if path.lower() == 'done':
-        if not folder_paths:
-            print("You haven't entered any folder paths yet!")
-            continue
-        else:
-            break
-
-    if path in folder_paths:
-        print("You've already added this folder path. Please add a different one.")
-        continue  # continue to next loop iteration
-
-    folder_paths.append(path)
-
+    raw_path = to_raw_string(path)
+    
+    if not os.path.exists(raw_path):
+        print("The provided path does not exist. Please enter a valid folder path.")
+        continue
+    
+    # Check if path has already been added
+    if raw_path in folder_paths:
+        print("You have already added this folder path. Please add a different one.")
+        continue
+    
+    folder_paths.append(raw_path)
 
 while True: # This outer loop allows for multiple keyword searches
     keyword = input("Enter the keyword to search for: ")
@@ -108,6 +116,24 @@ while True: # This outer loop allows for multiple keyword searches
 
     if not found:
         print(f"\nThe keyword '{keyword}' was not found in any .{file_extension} file.")
+    else:
+        open_choice = input("Do you want to open any of the matched files? (yes/no): ").lower()
+
+        if open_choice == 'yes':
+            while True:
+                filename_to_open = input("Enter the name of the file you want to open, 'all' to open all, or 'done' to finish: ")
+        
+                if filename_to_open.lower() == 'done':
+                    break
+
+                if filename_to_open.lower() == 'all':
+                    for match in matching_files:
+                        os.system(f'start "" "{match}"')  # Notice the extra quotes
+                    break
+                
+                for match in matching_files:
+                    if filename_to_open in os.path.basename(match):
+                        os.system(f'start "" "{match}"')  # Notice the extra quotes
 
     choice = input("Do you want to search again? If you want to change the folder paths, type 'change'. (yes/no/change): ").lower()
 
