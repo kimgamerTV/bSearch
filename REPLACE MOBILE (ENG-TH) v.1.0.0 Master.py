9705c9c10e00c496038eefd911098f8b6026a696
Bank <65855292+Bank848@users.noreply.github.com>
2023-10-07 17:14:37 +0700

    Update REPLACE MOBILE (ENG-TH) v.1.0.0 Master.py

--- REPLACE MOBILE (ENG-TH) v.1.0.0 Master.py
+++ REPLACE MOBILE (ENG-TH) v.1.0.0 Master.py
@@ -28,6 +28,10 @@ def atoi(text):
 def natural_keys(text):
     return [atoi(c) for c in re.split(r'(\d+)', text)]
 
+# Convert folder path to raw string
+def to_raw_string(s):
+    return s.encode('unicode_escape').decode()
+
 # Check if tqdm is installed, and if not, install it
 try:
     from tqdm import tqdm
@@ -61,9 +65,6 @@ print("ไม่ต้องแก้ที่อยู่โฟลเดอร�
 print("")
 print("Please set all Folder names to English first.")
 print("Enter the folder paths you want to search in. Type 'done' when finished:")
-
-def to_raw_string(s):
-    return s.encode('unicode_escape').decode()
 
 while True:
     path = input("Enter folder path (or 'done' to finish): ")
